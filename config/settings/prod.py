4c01b150a2b2e054e76b23b8aea7d860395e1e3b
meoyoug <eodyd426@gmail.com>
2024-12-17 10:25:15 +0900

    first commit (project_setting)

---
+++ config/settings/prod.py
@@ -0,0 +1,22 @@
+from dotenv import dotenv_values
+from config.settings.base import *
+
+ENV = dotenv_values('../../envs/.env.prod')
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ENV['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ENV['ALLOWED_HOSTS']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = 'static/'
