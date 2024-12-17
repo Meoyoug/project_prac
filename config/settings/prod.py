8702f33503e3343d65ca841937cd3b59245a0774
meoyoug <eodyd426@gmail.com>
2024-12-17 11:46:29 +0900

    add allow_host

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,7 +9,7 @@ SECRET_KEY = ENV['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ENV['ALLOWED_HOSTS']
+ALLOWED_HOSTS = ENV['ALLOWED_HOSTS'].split(', ')
 
 DATABASES = {
     'default': {
