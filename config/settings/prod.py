b23f58add80e79ae0112efea1886a10c84e0e6f3
meoyoug <eodyd426@gmail.com>
2024-12-17 13:44:27 +0900

    add static root

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -26,4 +26,4 @@ DATABASES = {
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '.staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
